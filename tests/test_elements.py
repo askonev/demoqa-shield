be3409d585234fe7ea3e07105305e9c340fa6f21
MaxSorokin <askonev007@gmail.com>
2025-09-05 17:50:30 +0300

    f: stat anal

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -4,7 +4,7 @@ from lib.ui.elements_page import ElementPage
 
 
 @pytest.mark.parametrize("url", ["https://demoqa.com/elements"])
-def test_page_headers_is_correct(page: Page, url):
+def test_page_headers_is_correct(page: Page, url: str):
     elements_page = ElementPage(page)
 
     elements_page.goto(url)
