957a4be67aaef441628deec208ee481d66473d3c
MaxSorokin <askonev007@gmail.com>
2025-09-24 18:53:46 +0300

    feature: text box submission

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,4 +1,5 @@
 import pytest
+from faker import Faker
 from playwright.sync_api import Page
 
 from lib.ui.home_page import HomePage
@@ -21,6 +22,19 @@ def test_elements_page_title_is_correct(elements: ElementsPage):
 
 
 def test_text_box_submission(elements: ElementsPage):
-    elements.dropdown_elements()
-    elements.goto_text_box()
-    print()
+    elements.dropdown_elements_click()
+    text_box = elements.goto_text_box()
+
+    fake = Faker()
+
+    data = {
+        "Full Name": fake.name(),
+        "Email": fake.email(),
+        "Current Address": fake.address(),
+        "Permanent Address": fake.address(),
+    }
+
+    text_box.set_text(data)
+    text_box.submit.click()
+    result = text_box.result_text()
+    assert "Name:" + data["Full Name"] == result
